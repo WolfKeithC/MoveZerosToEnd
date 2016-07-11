d9d59ea7d9981d556ec522b3b1c59f437ed3245c
Wolf <la_wolf@yahoo.com>
2016-07-11 13:12:28 -0700

    First Java Project

---
+++ src/MoveZerosToEndMain.java
@@ -0,0 +1,22 @@
+
+public class MoveZerosToEndMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, this is Keith. Welcome to my first JAVA app at Disney.");
+		
+		int[] arr01 = {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0};
+		DisplayArray(arr01);
+		System.out.println();
+		int[] arr02 = MoveZerosToEnd.move(arr01);
+		DisplayArray(arr02);
+	}
+	
+	public static void DisplayArray(int[] arr){
+		for(int i=0;i<arr.length;i++)
+		{
+			if(i > 0)
+				System.out.print(',');
+			System.out.print(arr[i]);
+		}
+	}
+}
