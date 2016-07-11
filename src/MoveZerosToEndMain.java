8a63512b02018b5d3144b51ac105dee002ed5a7d
Wolf <la_wolf@yahoo.com>
2016-07-11 14:20:37 -0700

    First SQL JAVA code

--- src/MoveZerosToEndMain.java
+++ src/MoveZerosToEndMain.java
@@ -1,3 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.*;
 
 public class MoveZerosToEndMain {
 
@@ -9,6 +14,8 @@ public class MoveZerosToEndMain {
 		System.out.println();
 		int[] arr02 = MoveZerosToEnd.move(arr01);
 		DisplayArray(arr02);
+		System.out.println();
+		ConnectAndDisplayDatabase();
 	}
 	
 	public static void DisplayArray(int[] arr){
@@ -17,6 +24,29 @@ public class MoveZerosToEndMain {
 			if(i > 0)
 				System.out.print(',');
 			System.out.print(arr[i]);
+		}
+	}
+	
+	public static void ConnectAndDisplayDatabase()
+	{
+		try
+		{		
+			String url = "jdbc:sqlserver://datglv-qdbw28.swna.wdpr.disney.com\\virtual2:2045;databaseName=ntcdb04;integratedSecurity=true";
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			Connection conn = DriverManager.getConnection(url);
+			System.out.println("connected");
+			Statement statement = conn.createStatement();
+	        String queryString = "select * from status where inactive_ind=0";
+	        ResultSet rs = statement.executeQuery(queryString);
+	        while (rs.next()) {
+	           System.out.println(rs.getString(1) + "\t" + rs.getString(2));
+	        }
+		}
+		catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
